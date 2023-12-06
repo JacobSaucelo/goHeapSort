3b97472f932ba75f774114c918beda2aaaf9a423
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-06 20:20:22 +0800

    fin

--- main.go
+++ main.go
@@ -17,6 +17,11 @@ func main() {
 		fmt.Println("new container: ", container)
 	}
 
+	for i := 0; i < len(container.slice); i++ {
+		container.Extract()
+		fmt.Println("extract", container)
+	}
+
 }
 
 func (h *Heap) Insert(key int) {
@@ -24,10 +29,53 @@ func (h *Heap) Insert(key int) {
 	h.MaxHeapUp(len(h.slice) - 1)
 }
 
+func (h *Heap) Extract() int {
+
+	if len(h.slice) == 0 {
+		fmt.Println("Empty slice")
+		return -1
+	}
+
+	extracted := h.slice[0]
+	l := len(h.slice) - 1
+
+	h.slice[0] = h.slice[l]
+	h.slice = h.slice[:l]
+
+	h.MaxHeapDown(0)
+
+	return extracted
+
+}
+
 func (h *Heap) MaxHeapUp(index int) {
 	for h.slice[Parent(index)] < h.slice[index] {
 		h.Swap(Parent(index), index)
 		index = Parent(index)
+	}
+}
+
+func (h *Heap) MaxHeapDown(index int) {
+	lastIndex := len(h.slice) - 1
+	l, r := left(index), right(index)
+	childToCompare := 0
+
+	for l <= lastIndex {
+		if l == lastIndex {
+			childToCompare = l
+		} else if h.slice[l] > h.slice[r] {
+			childToCompare = l
+		} else {
+			childToCompare = r
+		}
+
+		if h.slice[index] < h.slice[childToCompare] {
+			h.Swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		} else {
+			return
+		}
 	}
 }
 
